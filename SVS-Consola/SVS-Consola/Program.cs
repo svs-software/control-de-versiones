79e2066a75d7b5f617500502b83061dbc137bba0
PepoBJ <bj112143@gmail.com>
2017-10-02 19:50:06 -0500

    Mostrando Informacion

--- SVS-Consola/SVS-Consola/Program.cs
+++ SVS-Consola/SVS-Consola/Program.cs
@@ -22,6 +22,19 @@ namespace SVS_Consola
             P3.Id = "112125";
             P3.Nombre = "Elin";
 
+
+            Console.WriteLine("P1");
+            Console.WriteLine("ID: " + P1.Id);
+            Console.WriteLine("NOmber: " + P1.Nombre);
+
+            Console.WriteLine("P2");
+            Console.WriteLine("ID: " + P2.Id);
+            Console.WriteLine("NOmber: " + P2.Nombre);
+
+            Console.WriteLine("P3");
+            Console.WriteLine("ID: " + P3.Id);
+            Console.WriteLine("NOmber: " + P3.Nombre);
+
         }
     }
 }
